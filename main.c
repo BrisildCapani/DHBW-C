599b9e51b79c1b8c9554c6e6fa3ad0318d34c606
Brisild Capani <brisild.capani@aeb.com>
2024-12-19 12:13:50 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+const unsigned int THIS_IS_MY_CONST= 100u;
+
+int main(){
+
+    int aValue = 100;
+    const unsigned int CONST_2 = 200u; 
+
+    printf("Hello Wold");
+    return 0;
+}
